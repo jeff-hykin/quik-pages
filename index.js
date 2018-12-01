834c70aa04b0fcbfc8c4c0d6e523f3dfba55be25
Jeff Hykin <jeffhykin@jeffs-mbp-2.lan>
2018-12-01 17:46:24 -0600

    fixed subtle empty string bug

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ module.exports = thisModule = {
                 return (path) => {
                     // if no path given then use the current page path
                     if (path == null) {
-                        // path = the pathname minus the first slash
+                        // remove the first slash before calling the users load function
                         path = window.location.pathname.replace(/^\//,"")
                     }
                     window.history.pushState({url:path},"",path)
@@ -40,7 +40,7 @@ module.exports = thisModule = {
         // add the listener for back/forward buttons
         window.addEventListener("popstate", (e)=> {
             // if theres a loader and a url, then use them to load the page
-            if (window.pageManager.loadWithoutAddingToHistory && e.state.url) {
+            if (window.pageManager.loadWithoutAddingToHistory && e.state.url != null) {
                 window.pageManager.loadWithoutAddingToHistory(e.state.url)
             }
         })
